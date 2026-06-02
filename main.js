52c7bb6ef3aba298388d8ebb956b70ba67475009
Jayna Brisker <briskerjayna@gmail.com>
2026-06-02 14:11:50 -0700

    Created 2 Due application

---
+++ main.js
@@ -0,0 +1,198 @@
+//console.log("body before:", document.body.innerHTML);
+const tableBody = document.getElementById("taskTable");
+
+const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+const page = window.location.pathname;
+if (page.endsWith("index.html")) DisplayTask();
+
+if (page.endsWith("updateTask.html")) PopulateUpdatePage();
+
+tableBody.addEventListener("click", (e) => {
+  const tr = e.target.closest("tr");
+  if (!tr) return;
+  const previous = tableBody.querySelector(".selected");
+  if (previous) previous.classList.remove("selected");
+
+  tr.classList.add("selected");
+});
+
+/* A function written to populate the table
+with the tasks saved in localStorage
+param: newRow,newTask,newPriority,newStatus,taskItem
+return:
+*/
+function DisplayTask() {
+  // const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+  //create table row
+  for (let taskItem of taskList) {
+    const newRow = document.createElement("tr");
+    const newTask = document.createElement("td");
+    const newPriority = document.createElement("td");
+    const newStatus = document.createElement("td");
+
+    //add content
+    newTask.textContent = taskItem.task;
+    newPriority.textContent = taskItem.priority;
+    newStatus.textContent = taskItem.status;
+
+    try {
+      newRow.appendChild(newTask);
+      newRow.appendChild(newPriority);
+
+      newRow.appendChild(newStatus);
+      switch (taskItem.priority) {
+        case "High":
+          newRow.style.color = "red";
+          break;
+        case "Medium":
+          newRow.style.color = "orange";
+          break;
+        case "Low":
+          newRow.style.color = "green";
+          break;
+      }
+      tableBody.appendChild(newRow);
+    } catch (e) {
+      console.error(e.message);
+      return;
+    }
+  }
+}
+
+/*function used to create new tasks
+and append to list listen for add click
+params:taskName,priority,status
+return:
+*/
+function AddTask() {
+  const taskName = document.getElementById("tname").value.trim();
+  const priority = document.getElementById("taskPriority").value;
+  const status = document.getElementById("taskStatus").value;
+  //ensure input is valid
+  if (taskName === "") {
+    alert("Enter a task");
+    return;
+  }
+  //create new task
+  let newTask = {
+    task: taskName,
+    priority: priority,
+    status: status,
+  };
+  if (taskList.some((task) => task.task === taskName)) {
+    alert("Task already exists");
+    UpdateTask();
+    return;
+  }
+  taskList.push(newTask);
+  alert("Task Added");
+  //save to local storage
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+}
+
+/* function to delete tasks from list*/
+function DeleteTask() {
+  const selected = tableBody.querySelector(".selected");
+  if (!selected) {
+    alert("Select task to delete");
+    return;
+  }
+  //find index of selected row
+  const index = Array.from(tableBody.children).indexOf(selected);
+  //remove task
+  taskList.splice(index, 1);
+  // re-save task list
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+  tableBody.innerHTML = "";
+  DisplayTask();
+}
+
+// function to select which task to update
+function UpdateTask() {
+  //get selected task
+  const selected = tableBody.querySelector(".selected");
+
+  if (!selected) {
+    alert("Select task to update");
+    return;
+  }
+  const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+  const index = Array.from(tableBody.children).indexOf(selected);
+  //save task information in local storage
+  localStorage.setItem("taskItem", JSON.stringify(taskList[index]));
+  //navigate to update page
+  window.location.href = "updateTask.html";
+}
+
+//populate update page with selected task information
+function PopulateUpdatePage() {
+  const taskItem = JSON.parse(localStorage.getItem("taskItem"));
+
+  // populate update page with task information
+  const taskInput = document.getElementById("task");
+  taskInput.value = taskItem.task;
+
+  const priorityInput = document.getElementById("priority");
+  priorityInput.value = taskItem.priority;
+
+  const statusInput = document.getElementById("status");
+  statusInput.value = taskItem.status;
+
+  // save task information in local storage for use in SaveTask function
+  localStorage.setItem("taskToUpdate", JSON.stringify(taskItem));
+}
+
+//save updated task information in place of old task information
+function SaveTask() {
+  const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+  //updated task information
+  const updatedTask = {
+    task: document.getElementById("task").value.trim(),
+    priority: document.getElementById("priority").value,
+    status: document.getElementById("status").value,
+  };
+
+  const taskToUpdate = JSON.parse(localStorage.getItem("taskToUpdate"));
+
+  //find index of task to update
+  const index = taskList.findIndex((task) => task.task === taskToUpdate.task);
+
+  taskList[index] = updatedTask;
+
+  //remove updated task object from local storage
+  localStorage.removeItem("taskToUpdate");
+
+  //save updated task list to local storage
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+
+  //navigate back to main page
+  window.location.href = "index.html";
+}
+
+//complete task
+// function CompleteTask() {
+//   document.addEventListener("dblclick", (e) => {
+//     const taskList = JSON.parse(localStorage.getItem("taskList"));
+//     const selected = tableBody.querySelector(".selected");
+//     const index = Array.from(tableBody.children).indexOf(selected);
+//     if (taskList[index].status !== "Completed") return;
+//   });
+
+//   taskList.splice(index, 1);
+//   localStorage.setItem("taskList", JSON.stringify(taskList));
+//   DisplayTask();
+// }
+
+//clear list
+function ClearTasks() {
+  if (taskList.length === 0) {
+    alert("Nothing to Clear!");
+    return;
+  }
+  taskList.length = 0;
+  localStorage.clear();
+  alert("List Cleared");
+  taskTable.innerHTML = "";
+}
