0b9f6448fb9ac32b2c955acc1e80005a404c4081
Jayna Brisker <briskerjayna@gmail.com>
2026-06-02 18:24:33 -0700

    altered status input to be clickable progression

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-//console.log("body before:", document.body.innerHTML);
 const tableBody = document.getElementById("taskTable");
 
 const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 const page = window.location.pathname;
 if (page.endsWith("index.html")) DisplayTask();
-
-if (page.endsWith("updateTask.html")) PopulateUpdatePage();
 
 tableBody.addEventListener("click", (e) => {
   const tr = e.target.closest("tr");
@@ -16,14 +13,34 @@ tableBody.addEventListener("click", (e) => {
   tr.classList.add("selected");
 });
 
+//progress task with double click
+tableBody.addEventListener("dblclick", (e) => {
+  const tableRow = e.target.closest("tr");
+  if (!tableRow) return;
+
+  const index = Array.from(tableBody.children).indexOf(tableRow);
+
+  const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+  if (taskList[index].status === "Not Started") {
+    taskList[index].status = "In Progress";
+  } else if (taskList[index].status === "In Progress") {
+    tableRow.classList.add("completed");
+    taskList[index].status = "Completed";
+  } else if (taskList[index].status === "Completed") {
+    tableRow.classList.remove("completed");
+    taskList[index].status = "In Progress";
+  }
+  // re-save task list
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+});
+
 /* A function written to populate the table
 with the tasks saved in localStorage
 param: newRow,newTask,newPriority,newStatus,taskItem
 return:
 */
 function DisplayTask() {
-  // const taskList = JSON.parse(localStorage.getItem("taskList")) || [];
-
   //create table row
   for (let taskItem of taskList) {
     const newRow = document.createElement("tr");
@@ -82,7 +99,6 @@ function AddTask() {
   };
   if (taskList.some((task) => task.task === taskName)) {
     alert("Task already exists");
-    UpdateTask();
     return;
   }
   taskList.push(newTask);
@@ -124,7 +140,7 @@ function UpdateTask() {
   //navigate to update page
   window.location.href = "updateTask.html";
 }
-
+if (page.endsWith("updateTask.html")) PopulateUpdatePage();
 //populate update page with selected task information
 function PopulateUpdatePage() {
   const taskItem = JSON.parse(localStorage.getItem("taskItem"));
@@ -157,11 +173,24 @@ function SaveTask() {
   const taskToUpdate = JSON.parse(localStorage.getItem("taskToUpdate"));
 
   //find index of task to update
+  if (updatedTask.task === "") {
+    alert("Task name cannot be empty");
+    return;
+  }
+  if (taskToUpdate.task === null) {
+    alert("Task not found");
+    return;
+  }
   const index = taskList.findIndex((task) => task.task === taskToUpdate.task);
+  if (index === -1) {
+    alert("Task not found");
+    return;
+  }
 
   taskList[index] = updatedTask;
 
   //remove updated task object from local storage
+
   localStorage.removeItem("taskToUpdate");
 
   //save updated task list to local storage
@@ -170,20 +199,6 @@ function SaveTask() {
   //navigate back to main page
   window.location.href = "index.html";
 }
-
-//complete task
-// function CompleteTask() {
-//   document.addEventListener("dblclick", (e) => {
-//     const taskList = JSON.parse(localStorage.getItem("taskList"));
-//     const selected = tableBody.querySelector(".selected");
-//     const index = Array.from(tableBody.children).indexOf(selected);
-//     if (taskList[index].status !== "Completed") return;
-//   });
-
-//   taskList.splice(index, 1);
-//   localStorage.setItem("taskList", JSON.stringify(taskList));
-//   DisplayTask();
-// }
 
 //clear list
 function ClearTasks() {
